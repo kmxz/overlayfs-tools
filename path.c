001e8135eddf09c6482f96b03caacafd00f076c4
zhangyi (F) <yi.zhang@huawei.com>
2018-01-28 16:04:57 +0800

    fsck.overlay: correct joinname function and add comments
    Add comments for this function and fix some problems below:
    
    Before this patch:
    path\name |   /lib        ./lib       .
    ------------------------------------------
    /usr/	  |   /usr//lib   /usr/./lib  /usr/.
    /usr	  |   /usr/lib    /usr/./lib  /usr.
    ./usr	  |   ./usr/lib   ./usr/./lib ./usr.
    ./	  |   .//lib      ././lib     ./.
    ..        |   ../lib      .././lib    ...
    
    After this patch:
    path\name |   /lib        ./lib       .
    ------------------------------------------
    /usr/	  |   /usr/lib   /usr/lib     /usr/
    /usr	  |   /usr/lib   /usr/lib     /usr
    ./usr	  |   usr/lib    usr/lib      usr
    ./	  |   ./lib      ./lib        .
    ..        |   ../lib     ../lib       ..
    
    Signed-off-by: zhangyi (F) <yi.zhang@huawei.com>

--- path.c
+++ path.c
@@ -21,6 +21,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/*
+ * Join a null-terminated base directory path and a subdirectory path or
+ * filename into a pathname string.
+ *
+ * This function remove the duplicate '/' if base directory path end up
+ * with '/' and subpath start with another '/', and fill up the missing
+ * '/' otherwise. If either input path is the string "." or an empty
+ * string, returns the other one; if both input paths are empty string,
+ * then returns the string ".".
+ *
+ * This function not parse the string "./", "../" and duplicate '/' in
+ * the middle of input paths, and also not handle the case of NULL input,
+ * so need to make sure input correctly.
+ *
+ * The following list of examples shows the strings returned:
+ * 	path	name	join
+ * 	/usr	lib	/usr/lib
+ * 	/usr	.	/usr
+ * 	/usr	..	/usr/..
+ * 	.	lib	lib
+ * 	..	lib	../lib
+ * 	.       .       .
+ * 	..      ..      ../..
+ * 	...
+ *
+ * Note: This function will alloc memory, so should free the return value
+ *       after use.
+ *
+ * @path: base directory
+ * @name: subdirectory path or file name
+ */
 char *joinname(const char *path, const char *name)
 {
 	int lenp = strlen(path);
@@ -29,14 +61,24 @@ char *joinname(const char *path, const char *name)
 	int slash = 0;
 	char *str;
 
-	if (path[0] == '.' && lenp == 1)
+	if (path[0] == '.' && lenp == 1) {
+		path++;
 		lenp = 0;
-	if (name[0] == '.' && lenn == 1)
+	}
+	if (name[0] == '.' && lenn == 1) {
+		name++;
 		lenn = 0;
+	}
+
+	for (; name[0] == '.' && name[1] == '/'; name+=2, lenn-=2);
+	for (; path[0] == '.' && path[1] == '/'; path+=2, lenp-=2);
+	for (; name[0] == '/'; name++, lenn--);
 
 	len = lenp + lenn + 1;
-	if ((lenp > 0) && (path[lenp-1] != '/') &&
-	    (lenn > 0) && (name[0] != '/')) {
+	if (lenp == 0 && lenn == 0)
+		len++;
+	else if ((lenp > 0) && (lenn > 0) &&
+		 (path[lenp-1] != '/')) {
 		slash = 1;
 		len++;
 	}
@@ -45,12 +87,17 @@ char *joinname(const char *path, const char *name)
 	if (!str)
 		goto out;
 
-	memcpy(str, path, lenp);
-	if (slash) {
-		str[lenp] = '/';
-		lenp++;
+	if (lenp == 0 && lenn == 0) {
+		str[0] = '.';
+	} else {
+		memcpy(str, path, lenp);
+		if (slash) {
+			str[lenp] = '/';
+			lenp++;
+		}
+		memcpy(str+lenp, name, lenn);
 	}
-	memcpy(str+lenp, name, lenn+1);
+	str[len-1] = '\0';
 out:
 	return str;
 }
